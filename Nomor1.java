5a2312a1dedeb22628524cd0747aba55d3ae7fc8
Qorrykharismayani <melloflouscyz24@gmail.com>
2025-01-22 16:32:43 +0800

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,22 @@
-public class Nomor1{
-     public static void main(String[] args) {
-        
+import java.util.Scanner;
+
+public class Nomer1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in) ;
+
+        System.out.print ("Masukan total kilometer: ");
+        int totalKm = scanner.nextInt();
+
+        double honorTaksi;
+        if (totalKm <=3) {
+            honorTaksi = 20000.0; 
+        } else {
+            honorTaksi = 20000.0 + (totalKm - 3) * 10000.0;
+        }
+        System.out.println("Total KM: " + totalKm);
+        System.out.println("Honor Taksi: " + honorTaksi);
+
+       scanner.close();
+       
     }
 }
